7f36b32697ef4c9fdd42801bdd47e52a647eb2af
Phann Phearoen <phannphearin2@gmail.com>
2022-05-09 11:17:31 +0700

    add login layout

---
+++ layouts/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app :dark="setTheme">
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  computed: {
+    setTheme () {
+      this.$vuetify.theme.dark = false
+    },
+  }
+}
+</script>
