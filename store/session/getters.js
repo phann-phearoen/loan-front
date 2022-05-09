68a41e34835e90e235657de7a75f6f2f5eae2f53
Phann Phearoen <phannphearin2@gmail.com>
2022-05-09 11:17:44 +0700

    add vuex

--- store/session/getters.js
+++ store/session/getters.js
@@ -1,5 +1,4 @@
 export default {
-  // token과 user 둘다 검사 해야 한다. 코딩중 유저만 실수로 지우면 복구가 안되기때문
   isLoggedIn: (state) => !!state.token && !!state.user,
   getToken: (state) => state.token,
   getIpAddr: (state) => state.ip_addr || '',
