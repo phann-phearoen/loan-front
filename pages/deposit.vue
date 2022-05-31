0b45b702322d9f151f6d422027cafc948bdc5f92
Phann Phearoen <phannphearin2@gmail.com>
2022-05-31 22:33:47 +0700

    show sign after select member

--- pages/deposit.vue
+++ pages/deposit.vue
@@ -5,50 +5,60 @@
       <v-divider></v-divider>
       <v-card-subtitle>ជ្រើសរើសគណនីដែលត្រូវសន្សំ</v-card-subtitle>
       <v-card-text>
-        <v-form ref="form" lazy-validation>
-          <v-row>
-            <v-col cols="8">
-              <v-text-field
-                label="បញ្ចូលអត្តលេខសមាជិក"
-                hint="លេខរ៉ូម៉ាំង"
-                type="number"
-                v-model="memberId"
-                :rules="[v => !!v || 'សូមបញ្ចូលអត្តលេខឲ្យបានត្រឹមត្រូវ។']"
-              ></v-text-field>
-            </v-col>
-            <v-col>
-              <v-btn
-                :disabled="!memberId"
-                color="primary"
-                @click="doFindMember"
-              >ស្វែងរកសមាជិក</v-btn>
-            </v-col>
-          </v-row>
-          <v-row v-if="getOneMember">
-            <v-col cols="8">
-              <div id="foundMember">
+        <v-row>
+          <v-col cols="8">
+            <v-text-field
+              ref="memberId"
+              label="បញ្ចូលអត្តលេខសមាជិក"
+              hint="លេខរ៉ូម៉ាំង"
+              type="number"
+              v-model="memberId"
+              :rules="[v => !!v || 'សូមបញ្ចូលអត្តលេខឲ្យបានត្រឹមត្រូវ។']"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-btn
+              :disabled="!memberId"
+              color="primary"
+              @click="doFindMember"
+            >ស្វែងរកសមាជិក</v-btn>
+          </v-col>
+        </v-row>
+        <v-row v-if="getOneMember">
+          <v-col cols="8">
+            <div
+              id="foundMember"
+              :class="getOneMember === selectedMember ? 'is-selected' : ''"
+            >
+              <div class="d-inline mr-auto">
                 អត្តលេខ៖​ {{ getOneMember.id }}, 
                 ឈ្មោះ៖ {{ getOneMember.name }}, 
                 ភេទ៖ {{ getOneMember.gender }}
               </div>
-            </v-col>
-            <v-col>
-              <v-btn
-                color="primary"
-                @click="doSelectMember"
+              <div
+                v-if="getOneMember === selectedMember"
+                class="d-inline ml-auto"
               >
-                យកគណនីនេះ
-              </v-btn>
-              <v-btn
-                color="red"
-                dark
-                @click="doFindOtherMember"
-              >
-                ស្វែងរកថ្មី
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-form>
+                <v-icon color="#2196F3">mdi-check</v-icon>
+              </div>
+            </div>
+          </v-col>
+          <v-col>
+            <v-btn
+              color="primary"
+              @click="doSelectMember"
+            >
+              យកគណនីនេះ
+            </v-btn>
+            <v-btn
+              color="red"
+              dark
+              @click="doFindOtherMember"
+            >
+              ស្វែងរកថ្មី
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-card-text>
     </v-card>
   </v-container>
@@ -60,9 +70,7 @@ export default {
   data() {
     return {
       memberId: null,
-      selectedMember: {
-        id: null,
-      },
+      selectedMember: null,
     }
   },
   computed: {
@@ -76,6 +84,13 @@ export default {
         .then()
         .catch()
         .finally()
+    },
+    doSelectMember () {
+      this.selectedMember = this.getOneMember
+    },
+    doFindOtherMember() {
+      this.$store.commit('members/set_one_member', null)
+      this.$refs.memberId.$refs.input.focus()
     },
     async submit() {
       await this.$store
@@ -102,10 +117,14 @@ export default {
 
 <style scoped>
 #foundMember {
+  display: flex;
   width: 100%;
   border: 1px grey solid;
   border-radius: 5px;
   padding: .5em;
   font-size: 1.2em;
+}
+.is-selected {
+  border: 1px #2196F3 solid !important;
 }
 </style>
