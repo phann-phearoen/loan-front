0a6bfc2438bcb02d8b97b32b8bcbbc1c1adc165e
Phann Phearoen <phannphearin2@gmail.com>
2022-06-05 09:59:22 +0700

    add ConfirmDialog component

---
+++ components/ConfirmDialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog v-model="dialog.model" max-width="500">
+    <v-card width="100%">
+      <v-card-title>{{ dialog.title }}</v-card-title>
+      <v-divider></v-divider>
+      <v-card-text style="min-height: 150px">
+        <slot></slot>
+        {{ dialog.desc }}
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          color="red"
+          dark
+          min-width="100"
+          @click="confirmNo"
+        >
+          ទេ
+        </v-btn>
+        <v-btn
+          color="primary"
+          min-width="100"
+          @click="confirmYes"
+        >
+          បាទ/ចាស
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ['dialog'],
+  methods: {
+    confirmNo() { this.$nuxt.$emit('confirmNo') },
+    confirmYes() { this.$nuxt.$emit('confirmYes') },
+  },
+}
+</script>
