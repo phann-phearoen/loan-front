6dfde6eb3b8cd8bf5e2ee89a4a074ed68929f36b
Phann Phearoen <phannphearin2@gmail.com>
2022-06-11 10:30:03 +0700

    add loan card

--- pages/loan.vue
+++ pages/loan.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>loan</div>
+  <v-card>
+    <v-card-title class="justify-center">ផ្ដល់ប្រាក់កម្ចី</v-card-title>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-btn
+        color="primary"
+      >
+        សម្រាប់សមាជិក
+      </v-btn>
+      <v-btn
+        color="orange"
+        dark
+      >
+        សម្រាប់អតិថិជនក្រៅ
+      </v-btn>
+    </v-card-actions>
+    <v-divider></v-divider>
+    <v-card-text style="min-height: 200px">
+
+    </v-card-text>
+  </v-card>
 </template>
