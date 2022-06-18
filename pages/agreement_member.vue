cdd96184dd0d1898831f3cf0f1b00e3c1c7e9ca1
Phann Phearoen <phannphearin2@gmail.com>
2022-06-18 11:17:53 +0700

    display data of loanObject in agreement_member

--- pages/agreement_member.vue
+++ pages/agreement_member.vue
@@ -1,10 +1,39 @@
 <template>
   <v-card v-if="getLoanObject" width="100%">
     <v-card-text>
-      <div class="text-center">
+      <div class="text-center title">
         ព្រះរាជាណាចករកម្ពុជា<br>
-        ជាតិ សាសនា ព្រះមហាក្សត្រ
+        ជាតិ សាសនា ព្រះមហាក្សត្រ<br><br>
+        កិច្ចសន្យាខ្ចីចងការប្រាក់
       </div>
+      <div class="normal mt-12" style="min-height: 500px">
+        សមាជិកក្រុមអត្តលេខ {{ getLoanObject.member.id }}<br>
+        ឈ្មោះ {{ getLoanObject.member.name }}<br>
+        ចំនួន {{ getLoanObject.amount }} រៀល<br>
+        ការប្រាក់​ ១%<br>
+        រយៈពេល {{ getLoanObject.period }} ខែ។
+      </div>
+      <v-row
+        justify="center"
+        class="mt-12"
+        style="min-height: 200px"
+      >
+        <v-col>
+          <div class="text-center normal">
+            ហត្ថលេខាឬស្នាមមេដៃសាក្សី
+          </div>
+        </v-col>
+        <v-col>
+          <div class="text-center normal">
+            ហត្ថលេខាឬស្នាមមេដៃអ្នកខ្ចី
+          </div>
+        </v-col>
+        <v-col>
+          <div class="text-center normal">
+            ហត្ថលេខាឬស្នាមមេដៃតំណាងក្រុម
+          </div>
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
@@ -17,3 +46,14 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.title {
+  font-size: 1.5em;
+  font-weight: 600;
+}
+.normal {
+  font-size: 1.25em !important;
+  line-height: 2;
+}
+</style>
