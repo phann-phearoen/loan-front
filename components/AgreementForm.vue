b0d6d4f26a944de1c90bd2c977b62729c8c200eb
Phann Phearoen <phannphearin2@gmail.com>
2022-06-18 10:52:06 +0700

    add AgreementForm

---
+++ components/AgreementForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card width="100%">
+    <v-card-text>
+      <div class="text-center">
+        ព្រះរាជាណាចករកម្ពុជា<br>
+        ជាតិ សាសនា ព្រះមហាក្សត្រ
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['client', 'isMember', 'amount', 'period'],
+  computed: {
+    rate() {
+      return this.isMember ? 0.01 : 0.03
+    },
+  },
+}
+</script>
