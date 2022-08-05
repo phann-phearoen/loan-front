3927b52746effa795d47ef10d7268729a8e36ab9
Phann Phearoen <phannphearin2@gmail.com>
2022-08-05 11:02:41 +0700

    add data summary store

--- store/users/getters.js
+++ store/users/getters.js
@@ -1,3 +1,7 @@
 export default {
   getAllUsers: (state) => state.allUsers || [],
+  getTotalMembers: (state) => state.totalMembers || 0,
+  getTotalDeposits: (state) => state.totalDeposits || 0,
+  getTotalLoans: (state) => state.totalLoans || 0,
+  getTotalClients: (state) => state.totalClients || 0,
 }
