3990b771542120cb04a4436747bef4703a39859e
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2022-08-08 00:05:47 +0700

    member profile

--- pages/members/_id.vue
+++ pages/members/_id.vue
@@ -1,120 +1,87 @@
 <template>
-  <v-card width="100%" v-if="oneMember">
-    <v-card-title class="justify-center text-h5">
-      គណណីរបស់ {{ oneMember.name }}
-    </v-card-title>
-    <v-divider></v-divider>
-    <v-card-text class="px-16">
-      <v-row class="detail-row">
-        <v-col
-          class="text-h6"
-          cols="3"
-          align-self="end"  
-        >
-          ឈ្មោះ
-        </v-col>
-        <v-col
-          align-self="end"
-          class="text-h6"
-        >
-          {{ oneMember.name }}
-        </v-col>
-      </v-row>
+  <div>
+    <v-card width="100%" v-if="oneMember">
+      <v-card-title class="justify-center text-h5">
+        គណណីរបស់ {{ oneMember.name }}
+      </v-card-title>
       <v-divider></v-divider>
-      <v-row class="detail-row">
-        <v-col
-          class="text-h6"
-          cols="3"
-          align-self="end"  
-        >
-          ភេទ
-        </v-col>
-        <v-col
-          align-self="end"
-          class="text-h6"
-        >
-          {{ oneMember.gender }}
-        </v-col>
-      </v-row>
+      <v-card-text class="px-16">
+        <v-row class="detail-row">
+          <v-col class="text-h6" cols="3" align-self="end"> ឈ្មោះ </v-col>
+          <v-col align-self="end" class="text-h6">
+            {{ oneMember.name }}
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row class="detail-row">
+          <v-col class="text-h6" cols="3" align-self="end"> ភេទ </v-col>
+          <v-col align-self="end" class="text-h6">
+            {{ oneMember.gender }}
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row class="detail-row">
+          <v-col class="text-h6" cols="3" align-self="end">
+            ប្រាក់សន្សំសរុប
+          </v-col>
+          <v-col align-self="end" class="text-h6">
+            {{ oneMember.deposit }} ៛
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row class="detail-row">
+          <v-col class="text-h6" cols="3" align-self="end">
+            ប្រាក់កម្ចីសរុប
+          </v-col>
+          <v-col align-self="end" class="text-h6">
+            {{ oneMember.loan }} ៛
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn x-large color="primary" @click="toDeposit">
+          ដាក់ប្រាក់សន្សំ
+        </v-btn>
+        <v-btn x-large color="primary" @click="toRepay"> សងប្រាក់ </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card color="indigo">
+      <v-card-title class="justify-center text-h5">
+        {{ oneMember.name }}({{oneMember.gender}})
+      </v-card-title>
       <v-divider></v-divider>
-      <v-row class="detail-row">
-        <v-col
-          class="text-h6"
-          cols="3"
-          align-self="end"  
-        >
-          ប្រាក់សន្សំសរុប
-        </v-col>
-        <v-col
-          align-self="end"
-          class="text-h6"
-        >
-          {{ oneMember.deposit }} ៛
-        </v-col>
-      </v-row>
-      <v-divider></v-divider>
-      <v-row class="detail-row">
-        <v-col
-          class="text-h6"
-          cols="3"
-          align-self="end"  
-        >
-          ប្រាក់កម្ចីសរុប
-        </v-col>
-        <v-col
-          align-self="end"
-          class="text-h6"
-        >
-          {{ oneMember.loan }} ៛
-        </v-col>
-      </v-row>
-      <v-divider></v-divider>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-        x-large
-        color="primary"
-        @click="toDeposit"
-      >
-        ដាក់ប្រាក់សន្សំ
-      </v-btn>
-      <v-btn
-        x-large
-        color="primary"
-        @click="toRepay"
-      >
-        សងប្រាក់
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+      <v-row></v-row>
+    </v-card>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 export default {
   computed: {
-    id() { return this.$route.params.id },
-    ...mapGetters('members', { oneMember: 'getOneMember' }),
+    id() {
+      return this.$route.params.id;
+    },
+    ...mapGetters("members", { oneMember: "getOneMember" }),
   },
   methods: {
     async fetchOneMember() {
       await this.$store
-        .dispatch('members/getOneMember', this.id)
+        .dispatch("members/getOneMember", this.id)
         .then()
-        .finally()
+        .finally();
     },
     toDeposit() {
-      this.$store.commit('members/set_member_to_deposit', this.oneMember)
-      this.$router.push('/deposit')
+      this.$store.commit("members/set_member_to_deposit", this.oneMember);
+      this.$router.push("/deposit");
     },
-    toRepay() {
-      
-    },
+    toRepay() {},
   },
   mounted() {
-    this.fetchOneMember()
+    this.fetchOneMember();
   },
-}
+};
 </script>
 
 <style scoped>
